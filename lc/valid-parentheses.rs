d7359133ed71c0f2ec72fb028997d27a50588fab
Roscoe <45630405+roscoelai@users.noreply.github.com>
2021-07-27 10:42:32 +0800

    Update valid-parentheses.rs

--- lc/valid-parentheses.rs
+++ lc/valid-parentheses.rs
@@ -7,23 +7,22 @@ struct Solution;
 
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        if s.len() < 2 || s.len() % 2 != 0 { return false; }
+        if s.len() % 2 != 0 { return false; }
 
-        let mut chars: Vec<char> = Vec::new();
+        let mut stack = Vec::new();
 
         for x in s.chars() {
-            match chars.last() {
-                None => chars.push(x),
-                Some(v) => match v {
-                    '(' => if x == ')' { chars.pop(); } else { chars.push(x); },
-                    '[' => if x == ']' { chars.pop(); } else { chars.push(x); },
-                    '{' => if x == '}' { chars.pop(); } else { chars.push(x); },
-                    _ => { return false; },
-                },
+            match x {
+                '(' => stack.push(')'),
+                '[' => stack.push(']'),
+                '{' => stack.push('}'),
+                ')' | ']' | '}' if Some(x) != stack.pop() => return false,
+                ')' | ']' | '}' => (),
+                _ => return false,
             };
         }
 
-        chars.len() == 0
+        stack.is_empty()
     }
 }
 
@@ -32,44 +31,30 @@ mod tests {
     use super::*;
 
     fn helper(s: &str, want: bool) {
-        let s = s.to_string();
-        let got = Solution::is_valid(s);
+        let got = Solution::is_valid(s.to_string());
+        eprintln!("got is_valid('{}') = {}, want {}", s, got, want);
         assert_eq!(got, want);
     }
 
     #[test]
-    fn test_is_valid_example_1() {
-        helper("()", true);
-    }
+    fn test_is_valid_example_1() { helper("()", true); }
 
     #[test]
-    fn test_is_valid_example_2() {
-        helper("()[]{}", true);
-    }
+    fn test_is_valid_example_2() { helper("()[]{}", true); }
 
     #[test]
-    fn test_is_valid_example_3() {
-        helper("(]", false);
-    }
+    fn test_is_valid_example_3() { helper("(]", false); }
 
     #[test]
-    fn test_is_valid_example_4() {
-        helper("([)]", false);
-    }
+    fn test_is_valid_example_4() { helper("([)]", false); }
 
     #[test]
-    fn test_is_valid_example_5() {
-        helper("{[]}", true);
-    }
+    fn test_is_valid_example_5() { helper("{[]}", true); }
 
     #[test]
-    fn test_is_valid_example_6() {
-        helper("{{}[][[[]]]}", true);
-    }
+    fn test_is_valid_example_6() { helper("{{}[][[[]]]}", true); }
 
     #[test]
-    fn test_is_valid_example_7() {
-        helper("{{({})}}", true);
-    }
+    fn test_is_valid_example_7() { helper("{{({})}}", true); }
 
 }
