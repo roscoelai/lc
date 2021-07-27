02fddefdf646571dea78908bc20639972ad09579
Roscoe <45630405+roscoelai@users.noreply.github.com>
2021-07-27 09:14:18 +0800

    Rename some variables, add error message

--- lc/palindrome-number.rs
+++ lc/palindrome-number.rs
@@ -10,21 +10,18 @@ impl Solution {
         if x < 0 { return false; }
 
         let mut aux = x;
-        let mut carrys = (aux as f64).log10() as u32;
+        let mut characteristic = (x as f64).log10() as u32;
 
         while aux > 9 {
-            let tens = 10_i32.pow(carrys);
-            let first = aux / tens;
+            let antilog = 10_i32.pow(characteristic);
+            let first = aux / antilog;
             let last = aux % 10;
 
             if first != last { return false; }
+            if characteristic < 2 { break; }
 
-            aux = (aux - first * tens) / 10;
-            println!("carrys = {}", carrys);
-            carrys = match carrys.checked_sub(2) {
-                Some(v) => v,
-                None => break,
-            };
+            aux = (aux - first * antilog) / 10;
+            characteristic -= 2;
         }
 
         true
@@ -37,42 +34,26 @@ mod tests {
 
     fn helper(x: i32, want: bool) {
         let got = Solution::is_palindrome(x);
+        eprintln!("got is_palindrome({}) = {}, want {}", x, got, want);
         assert_eq!(got, want);
     }
 
     #[test]
-    fn test_is_palindrome_example_1() {
-        helper(121, true);
-    }
+    fn test_is_palindrome_example_1() { helper(121, true); }
 
     #[test]
-    fn test_is_palindrome_example_2() {
-        helper(-121, false);
-    }
+    fn test_is_palindrome_example_2() { helper(-121, false); }
 
     #[test]
-    fn test_is_palindrome_example_3() {
-        helper(10, false);
-    }
+    fn test_is_palindrome_example_3() { helper(10, false); }
 
     #[test]
-    fn test_is_palindrome_example_4() {
-        helper(-101, false);
-    }
+    fn test_is_palindrome_example_4() { helper(-101, false); }
 
     #[test]
-    fn test_is_palindrome_high_even() {
-        helper(2109889012, true);
-    }
+    fn test_is_palindrome_high_even() { helper(1234554321, true); }
 
     #[test]
-    fn test_is_palindrome_high_odd() {
-        helper(210989012, true);
-    }
-
-    #[test]
-    fn test_is_palindrome_zero() {
-        helper(0, true);
-    }
+    fn test_is_palindrome_high_odd() { helper(123454321, true); }
 
 }
