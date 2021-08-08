6321223093befd77ceabae70fc6a46f90a8c07a4
Roscoe <45630405+roscoelai@users.noreply.github.com>
2021-08-08 15:58:05 +0800

    Update tests
    Accept valid answers with different orders.

--- lc/two-sum.rs
+++ lc/two-sum.rs
@@ -1,9 +1,4 @@
-//! rustc two-sum.rs --test
-//! ./two-sum.exe
-
 use std::collections::HashMap;
-
-fn main() {}
 
 struct Solution;
 
@@ -12,13 +7,14 @@ impl Solution {
         let mut idxs = HashMap::new();
 
         for (j, num) in nums.iter().enumerate() {
-            match idxs.get(&(target - num)) {
+            let diff = target - num;
+            match idxs.get(&diff) {
                 None => idxs.insert(num, j as i32),
                 Some(i) => return vec![*i, j as i32],
             };
         }
 
-        panic!("Impossible to have no valid answers!");
+        panic!("Impossible. Guaranteed to have no valid answers.");
     }
 }
 
@@ -27,23 +23,19 @@ mod tests {
     use super::*;
 
     fn helper(nums: Vec<i32>, target: i32, want: Vec<i32>) {
-        let got = Solution::two_sum(nums, target);
+        let mut got = Solution::two_sum(nums.clone(), target);
+        got.sort();
+        eprintln!("two_sum({:?}, {:?}) = {:?}", nums, target, got);
         assert_eq!(got, want);
     }
 
     #[test]
-    fn test_two_sum_example_1() {
-        helper(vec![2, 7, 11, 15], 9, vec![0, 1]);
-    }
+    fn test_two_sum_1() { helper(vec![2, 7, 11, 15], 9, vec![0, 1]); }
 
     #[test]
-    fn test_two_sum_example_2() {
-        helper(vec![3, 2, 4], 6, vec![1, 2]);
-    }
+    fn test_two_sum_2() { helper(vec![3, 2, 4], 6, vec![1, 2]); }
 
     #[test]
-    fn test_two_sum_example_3() {
-        helper(vec![3, 3], 6, vec![0, 1]);
-    }
+    fn test_two_sum_3() { helper(vec![3, 3], 6, vec![0, 1]); }
 
 }
