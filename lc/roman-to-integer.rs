31e0b0f393adb8660626588e6c55902026444302
Roscoe <45630405+roscoelai@users.noreply.github.com>
2021-08-08 15:54:02 +0800

    Some tidying

--- lc/roman-to-integer.rs
+++ lc/roman-to-integer.rs
@@ -1,22 +1,20 @@
-//! rustc roman-to-integer.rs --test
-//! ./roman-to-integer.exe
-
-fn main() {}
-
 struct Solution;
 
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
-        let mut nums: Vec<i32> = s.chars().map(|x| match x {
-            'I' => 1,
-            'V' => 5,
-            'X' => 10,
-            'L' => 50,
-            'C' => 100,
-            'D' => 500,
-            'M' => 1000,
-            _ => panic!("Invalid Roman numeral: '{}'", s),
-        }).collect();
+        let mut nums: Vec<i32> = s
+            .chars()
+            .map(|x| match x {
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+                _ => panic!("Invalid Roman numeral: '{}'", s),
+            })
+            .collect();
 
         for i in 1..s.len() {
             if nums[i - 1] < nums[i] {
@@ -33,45 +31,30 @@ mod tests {
     use super::*;
 
     fn helper(s: &str, want: i32) {
-        let s = s.to_string();
-        let got = Solution::roman_to_int(s);
+        let got = Solution::roman_to_int(s.to_string());
         assert_eq!(got, want);
     }
 
     #[test]
-    fn test_roman_to_int_example_1() {
-        helper("III", 3);
-    }
+    fn test_roman_to_int_1() { helper("III", 3); }
 
     #[test]
-    fn test_roman_to_int_example_2() {
-        helper("IV", 4);
-    }
+    fn test_roman_to_int_2() { helper("IV", 4); }
 
     #[test]
-    fn test_roman_to_int_example_3() {
-        helper("IX", 9);
-    }
+    fn test_roman_to_int_3() { helper("IX", 9); }
 
     #[test]
-    fn test_roman_to_int_example_4() {
-        helper("LVIII", 58);
-    }
+    fn test_roman_to_int_4() { helper("LVIII", 58); }
 
     #[test]
-    fn test_roman_to_int_example_5() {
-        helper("MCMXCIV", 1994);
-    }
+    fn test_roman_to_int_5() { helper("MCMXCIV", 1994); }
 
     #[test]
-    fn test_roman_to_int_min() {
-        helper("I", 1);
-    }
+    fn test_roman_to_int_min() { helper("I", 1); }
 
     #[test]
-    fn test_roman_to_int_max() {
-        helper("MMMCMXCIX", 3999);
-    }
+    fn test_roman_to_int_max() { helper("MMMCMXCIX", 3999); }
 
     #[test]
     #[should_panic(expected = "Invalid Roman numeral: 'MMMCMZXCIX'")]
