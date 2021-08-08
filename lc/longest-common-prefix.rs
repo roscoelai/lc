1ed88b198163978bfd380fa7fcdf68e8f2ee2333
Roscoe <45630405+roscoelai@users.noreply.github.com>
2021-08-08 15:48:29 +0800

    Replace with simpler algorithm

--- lc/longest-common-prefix.rs
+++ lc/longest-common-prefix.rs
@@ -1,31 +1,23 @@
-//! rustc longest-common-prefix.rs --test
-//! ./longest-common-prefix.exe
-
-fn main() {}
-
 struct Solution;
 
 impl Solution {
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
-        let shortest: &str = strs
-            .iter()
-            .reduce(|a, b| if a.len() < b.len() { a } else { b })
-            .unwrap();
+        let n = strs.iter().map(|x| x.len()).min().unwrap();
+        let mut res = String::with_capacity(n);
 
-        let idx = Solution::last_idx(&strs, shortest);
+        for i in 0..n {
+            let c = &strs[0][i..=i];
 
-        shortest[..idx].to_string()
-    }
-
-    fn last_idx(strs: &Vec<String>, shortest: &str) -> usize {
-        let mut idx = 0;
-        for (i, a) in shortest.chars().enumerate() {
-            for s in strs {
-                if s.chars().nth(i).unwrap() != a { return idx; }
+            for j in 1..strs.len() {
+                if &strs[j][i..=i] != c {
+                    return res;
+                }
             }
-            idx += 1;
+
+            res.push_str(c);
         }
-        idx
+
+        res
     }
 }
 
@@ -35,17 +27,18 @@ mod tests {
 
     fn helper(strs: Vec<&str>, want: &str) {
         let strs: Vec<String> = strs.iter().map(|x| x.to_string()).collect();
-        let got = Solution::longest_common_prefix(strs);
+        let got = Solution::longest_common_prefix(strs.clone());
+        eprintln!("longest_common_prefix({:?}) = {:?}", strs, got);
         assert_eq!(got, want);
     }
 
     #[test]
-    fn test_longest_common_prefix_example_1() {
+    fn test_longest_common_prefix_1() {
         helper(vec!["flower", "flow", "flight"], "fl");
     }
 
     #[test]
-    fn test_longest_common_prefix_example_2() {
+    fn test_longest_common_prefix_2() {
         helper(vec!["dog", "racecar", "car"], "");
     }
 
